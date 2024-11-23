9e8bcc937e44af82a7dea7ae0ee2781d2e51f6d7
KHALED AHAMED.S <98593343+khaledahamed8080@users.noreply.github.com>
2024-11-23 15:24:01 +0530

    Path Variable

--- Spring-Hibernate-JPA/web/web/src/main/java/com/springboot/web/Controller/StudentRestController.java
+++ Spring-Hibernate-JPA/web/web/src/main/java/com/springboot/web/Controller/StudentRestController.java
@@ -4,22 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.web.Entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-      
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> stu=new ArrayList<>();
+      List<Student> stu;
+
+
+      @PostConstruct
+      public void load(){
+       stu=new ArrayList<>();
         stu.add(new Student("khaled","ahamed"));
         stu.add(new Student("farzana","begum"));
         stu.add(new Student("ghouse","basha"));
+      }
+
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
 
         return stu;
+    }
+
+    @GetMapping("/students/{studentid}")
+    public Student getStudents(@PathVariable int studentid){
+
+        return stu.get(studentid);
     }
 }
